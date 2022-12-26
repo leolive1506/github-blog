c38dd0d083f0edeeadaf4b21376fe9646143c749
Leonardo Lopes <leonardolivelopes2@gmail.com>
2022-12-26 15:37:49 -0300

    create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from "styled-components";
 import { Router } from "./Router";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <Router />
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   )
 }
